99da108bf469e3aa057166639b7bcc4d7e43423a
Naren404 <narendrasunar47@gmail.com>
2025-01-02 16:29:06 +1100

    why choose us added

--- components/Feature.js
+++ components/Feature.js
@@ -3,47 +3,38 @@ import React, { useMemo } from "react";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
-
-const features = [
-  "Powerfull online protection.",
-  "Internet without borders.",
-  "Supercharged VPN",
-  "No specific time limits."
-]
+import { FEATURES } from "../constants/features";
 
 const Feature = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
 
   return (
     <div
-      className="max-w-screen-xl mt-8 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+      className="max-w-screen-xl px-6 mx-auto mt-8 mb-6 sm:mt-14 sm:mb-14 sm:px-8 lg:px-16"
       id="feature"
     >
-      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
-        <ScrollAnimationWrapper className="flex w-full justify-end">
-          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
+      <div className="grid grid-flow-row grid-cols-1 gap-8 my-12 sm:grid-flow-col sm:grid-cols-2 p y-8">
+        <ScrollAnimationWrapper className="flex justify-end w-full">
+          <motion.div className="w-full h-full p-4" variants={scrollAnimation}>
             <Image
               src="/assets/Illustration2.png"
               alt="VPN Illustrasi"
               layout="responsive"
               quality={100}
-              height={414}
+              height={350}
               width={508}
             />
           </motion.div>
         </ScrollAnimationWrapper>
         <ScrollAnimationWrapper>
 
-        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
-          <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
-            We Provide Many Features You Can Use
+        <motion.div className="flex flex-col items-end justify-center w-full ml-auto lg:w-9/12" variants={scrollAnimation}>
+          <h3 className="text-3xl font-medium leading-relaxed lg:text-4xl text-black-600">
+            WHY CHOOSE BRIGHT IT HUB
           </h3>
-          <p className="my-2 text-black-500">
-            You can explore the features that we provide with fun and have their
-            own functions each feature.
-          </p>
-          <ul className="text-black-500 self-start list-inside ml-8">
-            {features.map((feature, index) => (
+          
+          <ul className="self-start mt-8 ml-8 list-inside text-black-500">
+            {FEATURES.map((feature, index) => (
               <motion.li
                 className="relative circle-check custom-list"
                 custom={{duration: 2 + index}}
---
+++ constants/features.js
@@ -0,0 +1,12 @@
+export const FEATURES = [
+  "24/7 IT Support",
+  "Tailored IT Solutions",
+  "Certified IT Experts",
+  "Reliable & Scalable Services",
+  "Seamless Technology Integration",
+  "Innovative Problem-Solving",
+  "Client-Focused Approach",
+  "Fast & Efficient Response Times",
+  "Fast On-Site & Remote Support",
+  "Real-Time Troubleshooting & Support",
+];
