31f9a5ffc99baaf769150a5b858ccf5988dd37f7
Naren404 <narendrasunar47@gmail.com>
2025-01-02 16:08:09 +1100

    chatbot added

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,26 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap"
             rel="stylesheet"
           />
+
+          {/* <!--Start of Tawk.to Script--> */}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                  s1.async=true;
+                  s1.src='https://embed.tawk.to/6776138caf5bfec1dbe5a5da/1igijonad';
+                  s1.charset='UTF-8';
+                  s1.setAttribute('crossorigin','*');
+                  s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+            }}
+          />
+          {/* <!--End of Tawk.to Script--> */}
+          
         </Head>
         <body>
           <Main />
